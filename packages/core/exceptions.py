7f4e98f30b8efb346cd8f9fbd5e1cb20746ec632
Sanjay G <gokulesh2615@gmail.com>
2026-08-11 16:10:56 +0530

    fix(executor): added timeout handling to prevent infinite waits in distributed task scheduler

--- packages/core/exceptions.py
+++ packages/core/exceptions.py
@@ -5,3 +5,8 @@ class EngineError(DIMOException):
     def __init__(self, message: str):
         self.message = message
         super().__init__(message)
+
+class TimeoutError(DIMOException):
+    def __init__(self, timeout_duration: int):
+        self.timeout_duration = timeout_duration
+        super().__init__(f'Timeout after {timeout_duration} seconds')
